85d33c37093a63383eba8bd765e081099893dad0
R.Noto <rikitonoto@gmail.com>
2023-05-23 19:06:30 +0900

    create colon compornent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import SevenSegment from './components/SevenSegment.vue'
+import Dot from './components/Dot.vue'
+import Colon from './components/Colon.vue';
 </script>
 
 <template>
   <SevenSegment :number=8 :height=350 :width=200></SevenSegment>
+  <Colon></Colon>
 </template>
 
 <style scoped>
---
+++ src/components/Colon.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Dot from './Dot.vue';
+
+const props = defineProps({
+  height: {
+    type: Number,
+    default: 350,
+  },
+});
+
+const offset: number = props.height * 75 / 350;
+const center: number = props.height / 2;
+</script>
+
+<template>
+  <div class="colon">
+    <Dot class="colon__dot__top"></Dot>
+    <Dot class="colon__dot__bottom"></Dot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .colon {
+    --offset: v-bind(offset);
+    --center: v-bind(center);
+    position: relative;
+    &__dot__top {
+      position: absolute;
+      top: calc(1px * calc(var(--center) - var(--offset)));
+    }
+
+    &__dot__bottom {
+      position: absolute;
+      top: calc(1px * calc(var(--center) + var(--offset)));
+    }
+  }
+</style>
