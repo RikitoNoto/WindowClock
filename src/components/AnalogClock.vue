9c1ce5b54e3670da746dca92c82f133897a2dc3a
R.Noto <rikitonoto@gmail.com>
2023-06-21 22:43:50 +0900

    create compornent

---
+++ src/components/AnalogClock.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="analog_clock">
+    <img src="../assets/clock.png"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.analog_clock {
+}
+</style>
--- src/components/DigitalClock.vue
+++ src/components/DigitalClock.vue
@@ -45,17 +45,17 @@ const char_width: number = char_height * 4/7;
 </script>
 
 <template>
-  <div class="display">
-    <SevenSegment class="display__char" :number=TimeUtil.numToDigArray(hour)[0] :height=char_height :width=char_width></SevenSegment>
-    <SevenSegment class="display__char" :number=TimeUtil.numToDigArray(hour)[1] :height=char_height :width=char_width></SevenSegment>
-    <Colon class="display__char" :height=char_height :turn_on_colon=true></Colon>
-    <SevenSegment class="display__char" :number=TimeUtil.numToDigArray(minute)[0] :height=char_height :width=char_width></SevenSegment>
-    <SevenSegment class="display__char" :number=TimeUtil.numToDigArray(minute)[1] :height=char_height :width=char_width></SevenSegment>
+  <div class="digital_clock">
+    <SevenSegment class="digital_clock__char" :number=TimeUtil.numToDigArray(hour)[0] :height=char_height :width=char_width></SevenSegment>
+    <SevenSegment class="digital_clock__char" :number=TimeUtil.numToDigArray(hour)[1] :height=char_height :width=char_width></SevenSegment>
+    <Colon class="digital_clock__char" :height=char_height :turn_on_colon=true></Colon>
+    <SevenSegment class="digital_clock__char" :number=TimeUtil.numToDigArray(minute)[0] :height=char_height :width=char_width></SevenSegment>
+    <SevenSegment class="digital_clock__char" :number=TimeUtil.numToDigArray(minute)[1] :height=char_height :width=char_width></SevenSegment>
   </div>
 </template>
 
 <style scoped lang="scss">
-.display {
+.digital_clock {
   &__char {
     display: inline-block;
     margin-left: 1vw;
