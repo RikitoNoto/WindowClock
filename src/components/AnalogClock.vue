a4457bdc7694ade261e2085abe7316ca38b1b8a3
R.Noto <rikitonoto@gmail.com>
2023-06-22 19:15:28 +0900

    create clock hands and a center circle

--- src/components/AnalogClock.vue
+++ src/components/AnalogClock.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
+import Hand from './Hand.vue';
 </script>
 
 <template>
   <div class="analog_clock">
     <img src="../assets/clock.png" class="analog_clock__compass"/>
-    <div class="analog_clock__hand_hour">
-      <div class="analog_clock__hand_hour__dest"></div>
-    </div>
-
-    <div class="analog_clock__hand_minute">
-      <div class="analog_clock__hand_minute__dest"></div>
-    </div>
+    <div class="analog_clock__center_circle"></div>
+    <Hand class="analog_clock__hand_hour" :height="500" :width="250" :weight="25" :rotate="0" color="#3650FF"></Hand>
+    <Hand class="analog_clock__hand_min" :height="700" :width="250" :weight="25" :rotate="0" color="#A6FF69"></Hand>
+    <Hand class="analog_clock__hand_sec" :height="700" :width="250" :weight="10" :rotate="0" color="coral"></Hand>
   </div>
 </template>
 
@@ -22,69 +20,40 @@
     height: 100vh;
     top: calc(50% - 100vh / 2);
     left: calc(50% - 100vh / 2);
+    z-index: 0;
+  }
+
+  &__center_circle {
+    --circle-radius: 35px;
+    position:absolute;
+    top: calc(50% - var(--circle-radius) / 2);
+    left: calc(50% - var(--circle-radius) / 2 - 2px);
+    height: var(--circle-radius);
+    width: var(--circle-radius);
+    border-radius: 50%;
+    background-color: black;
+    z-index: 20;
   }
 
   &__hand_hour {
-    --hour_hand_height: 500px;
-    --hour_hand_width: 250px;
-    --hour_hand_weight: 50px;
-
     position:absolute;
-    width: var(--hour_hand_width);
-    height: var(--hour_hand_height);
-    top: calc(50% - var(--hour_hand_height) / 2);
-    left: calc(50% - var(--hour_hand_width) / 2 - 1px);
-
-    &__dest {
-      display: flex;
-      justify-content: center;
-      position: absolute;
-      border-radius: 50%;
-      width: var(--hour_hand_width);
-      height: var(--hour_hand_height);
-      &::before{
-        content: '';
-        position: absolute;
-        width: var(--hour_hand_weight);
-        height: calc(var(--hour_hand_height) / 2);
-        background: black;
-        z-index: 11;
-        border-radius: calc(var(--hour_hand_weight) / 2);
-      }
-    }
+    top: calc(50% - 500px / 2);
+    left: calc(50% - 250px / 2 - 2px);
+    z-index: 3;
   }
 
-&__hand_minute {
-  --minute_hand_height: 700px;
-  --minute_hand_width: 250px;
-  --minute_hand_weight: 25px;
-
-  position:absolute;
-  width: var(--minute_hand_width);
-  height: var(--minute_hand_height);
-  top: calc(50% - var(--minute_hand_height) / 2);
-  left: calc(50% - var(--minute_hand_width) / 2 - 1px);
-
-  &__dest {
-    display: flex;
-    justify-content: center;
-    position: absolute;
-    border-radius: 50%;
-    width: var(--minute_hand_width);
-    height: var(--minute_hand_height);
-    rotate: 90deg;
-    &::before{
-      content: '';
-      position: absolute;
-      width: var(--minute_hand_weight);
-      height: calc(var(--minute_hand_height) / 2);
-      background: black;
-      z-index: 11;
-      border-radius: calc(var(--minute_hand_weight) / 2);
-    }
+  &__hand_min {
+    position:absolute;
+    top: calc(50% - 700px / 2);
+    left: calc(50% - 250px / 2 - 2px);
+    z-index: 2;
   }
-}
 
-
+  &__hand_sec {
+    position:absolute;
+    top: calc(50% - 700px / 2);
+    left: calc(50% - 250px / 2 - 2px);
+    z-index: 4;
+  }
 }
 </style>
